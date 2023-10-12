7a4b2edcb91c8d1deeb6adaa469723d8ffd6e808
zmancekpali <145681961+zmancekpali@users.noreply.github.com>
2023-10-12 12:55:06 +0100

    week 4

--- Coding Club 1/Week 4/Data manipulation script.R
+++ Coding Club 1/Week 4/Data manipulation script.R
@@ -7,7 +7,8 @@
 #Data manipulation (https://ourcodingclub.github.io/tutorials/data-manip-intro/) ----
 
 #Setting a working directory 
-setwd("file-path") #will not set one here because it is connected to Github
-bla 
- 
+setwd("/Users/zojamancekpali/DS - Personal repo - Zoja/Coding Club 1/Week 4")
+elongation <- read.csv("EmpetrumElongation.csv", header = TRUE) 
+
+
 #Efficient data manipulation (https://ourcodingclub.github.io/tutorials/data-manip-efficient/) ----
