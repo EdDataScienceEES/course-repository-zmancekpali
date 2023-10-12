08cb617bb8165e5a21bba33c876622e4c1fa6fbf
zmancekpali <145681961+zmancekpali@users.noreply.github.com>
2023-10-12 12:09:36 +0100

    bla

---
+++ Coding club /W4 - Data manipulation script.R
@@ -0,0 +1,13 @@
+##%#########################################################################%##
+#                                                                             #
+#                         Coding club W4 (11.10.2023)                         #
+#                                                                             #
+##%#########################################################################%##
+
+#Data manipulation (https://ourcodingclub.github.io/tutorials/data-manip-intro/) ----
+
+#Setting a working directory 
+setwd("file-path") #will not set one here because it is connected to Github
+bla 
+ 
+#Efficient data manipulation (https://ourcodingclub.github.io/tutorials/data-manip-efficient/) ----
