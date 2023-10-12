0ed18308525d8508afd65723caa592c2ce852f66
zmancekpali <145681961+zmancekpali@users.noreply.github.com>
2023-10-12 14:15:13 +0100

    changes + W4 readings (started)

--- Weekly DS content/DS-W4.R
+++ Weekly DS content/DS-W4.R
@@ -4,4 +4,15 @@
 #                                                                             #
 ##%#########################################################################%##
 
+#Good practice for data wrangling
+#First, organise the input data: each row is a separate observation and each variable has a separate column
+    #No merged cells, special characters, or colours; definitely in csv or txt format
+#Next, tidy your data: 
+    #Often have to change from a wide to a long format (so you can analyse it)
+    #Sort names (keep naming style consistent; I'll use name_name)
+    #Make sure columns are in the correct format (numeric, character, or factor)
+    #Figure out workflow
+
+
+
 
