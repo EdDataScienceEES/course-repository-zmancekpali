21dcc44657240913105750222708cf1438e739b4
zmancekpali <145681961+zmancekpali@users.noreply.github.com>
2023-10-19 15:42:29 +0100

    challenge 2 - start

---
+++ Data Science Challenges/Challenge 2/Challenge 2.R
@@ -0,0 +1,14 @@
+##%#########################################################################%##
+#                                                                             #
+#                           Data science Challenge 2                          #
+#                               Zoja Manček Páli                              #
+#                                                                             #
+##%#########################################################################%##
+
+#Group assignment (assign roles - issues about the meeting, explain the roles in the README, etc.)- 
+#1. Find a story with publicly available data (ecological ideally)
+#2. Develop key message
+#3. Build and present a workflow
+#4. One figure that's appropriate to be presented (easy to read, good colours, include a caption)
+
+
