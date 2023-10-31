2c0764f3006a30d0649c772e2cc6f83cffdc3bf5
zmancekpali <145681961+zmancekpali@users.noreply.github.com>
2023-10-31 13:58:12 +0000

    challenge 2 ideas

--- Data Science Challenges/Challenge 2/Challenge 2.R
+++ Data Science Challenges/Challenge 2/Challenge 2.R
@@ -1,64 +1,64 @@
-##%#########################################################################%##
-#                                                                             #
-#                           Data science Challenge 2                          #
-#                               Zoja Manček Páli                              #
-#                                                                             #
-##%#########################################################################%##
+#here is what I managed to do with the map - it gives all of the most common car 
+#makes for each WA county and it looks p good I think. 
 
-#Group assignment (assign roles - issues about the meeting, explain the roles in the README, etc.)- 
-#1. Find a story with publicly available data (ecological ideally)
-#2. Develop key message
-#3. Build and present a workflow
-#4. One figure that's appropriate to be presented (easy to read, good colours, include a caption)
-
-#WD
-setwd("~/") #erases previously set WDs
-setwd("DS - Personal repo - Zoja/Data Science Challenges/Challenge 2") #sets a new one
-getwd() #check that it's worked
-
+#I was thinking we could compare this (map1) with the all-US one (map2) to see?
+#Also these are very rough - just to give an idea
 
 #Libraries
+library(ggmap)
 library(tibble)
 library(sf)
 library(tidyverse)
 
 #Data
 cars <- read.csv("Electric_Vehicle_Population_Data.csv") #data from https://catalog.data.gov/dataset/electric-vehicle-population-data
-capitals <- read.csv("us-state-capitals.csv") #data from https://github.com/jasperdebie/VisInfo/blob/master/us-state-capitals.csv
 
 #Inspection 
 head(cars)
 str(cars)
 
-length(unique(new_cars$State)) #43 states total
-length(unique(new_cars$Make)) #37 makes of cars
-length(unique(new_cars$Electric.Vehicle.Type)) #2 types (BEV or PHEV)
-
 #Wrangling
 #Tidying the data ----
-new_cars <- cars %>% 
-  mutate(Vehicle.Location = gsub("POINT \\((-?\\d+\\.\\d+) (-?\\d+\\.\\d+)\\)", "\\1,\\2", Vehicle.Location), #removes the POINT and () around the longitude and latitude
-         Vehicle.Location = ifelse(Vehicle.Location == "", NA, Vehicle.Location)) %>% # adds NAs for missing location values
+#Washington only
+county_coordinates <- cars %>%
+  filter(State == "WA") %>%
+  mutate(Vehicle.Location = gsub("POINT \\((-?\\d+\\.\\d+) (-?\\d+\\.\\d+)\\)", "\\1,\\2", Vehicle.Location), # removes the POINT and () around the longitude and latitude
+         Vehicle.Location = ifelse(Vehicle.Location == "", NA, Vehicle.Location)) %>% # adds NA for missing location values
   separate(Vehicle.Location, into = c("Longitude", "Latitude"), sep = ",") %>% # splits the values into latitude and longitude
-  filter(State != "BC" & State != "AP") %>% #remove the Non-US state rows 
+  select(County, Longitude, Latitude) %>%  # select only the relevant columns
   mutate(Latitude = as.numeric(Latitude),
          Longitude = as.numeric(Longitude)) # changes longitude and latitude values to numeric
 
-#Most common make and type of car for each state:
-common_car <- new_cars %>%
-  group_by(State, Make, Electric.Vehicle.Type) %>%
-  summarise(Count = n()) %>%
-  arrange(State, desc(Count)) %>%
+washington_cars <- cars %>%
+  filter(State == "WA") %>%
+  group_by(County, Make) %>% # group by the county and the make of the car
+  summarise(Count = n()) %>% # count the number of occurrences of each make in each county
+  arrange(County, desc(Count)) %>% # select the car with the highest number of occurrences in each county
   slice(1) %>%
   ungroup()
 
-#State capital coordinates ----
+joined <- left_join(washington_cars, county_coordinates, by = "County") %>% 
+  group_by(County) %>%
+  arrange(County, desc(Count)) %>%
+  slice(1) %>%
+  ungroup() #final data for plotting
+
+# All of the US
+us_cars <- cars %>%
+  filter(State != "BC" & State != "AP") %>% # remove the Non-US state rows 
+  group_by(State, Make) %>% # group by the county and the make of the car
+  summarise(Count = n()) %>% # count the number of occurrences of each make in each county
+  arrange(State, desc(Count)) %>% # select the car with the highest number of occurrences in each county
+  slice(1) %>%
+  ungroup()
+
+#State capital coordinates
 state_capitals <- data.frame(
-  State = c("WA", "BC", "IN", "NH", "VA", "NC", "GA", "CA", "NE", "CO", "NY", 
+  State = c("WA", "IN", "NH", "VA", "NC", "GA", "CA", "NE", "CO", "NY", 
             "MD", "DC", "AL", "LA", "IL", "TX", "FL", "AZ", "OH", "HI", "CT", 
             "MA", "OR", "NJ", "IA", "SC", "AR", "ID", "NV", "OK", "MN", "UT", 
             "AP", "AK", "KY", "MO", "PA", "MT", "KS", "WY", "MS", "DE"),
-  cap_lat = c(47.6097, 39.7684, 43.2081, 37.54, 35.7796, 33.749, 
+  cap_lat = c(47.6097, 43.2081, 37.54, 35.7796, 33.749, 
               38.8951, 40.8136, 39.7392, 42.6526, 38.9787, 38.8951, 
               32.8067, 30.6954, 30.4583, 39.7817, 30.2672, 30.4383, 
               33.4484, 39.9612, 21.3045, 41.7658, 42.3601, 44.06, 
@@ -66,7 +66,7 @@ state_capitals <- data.frame(
               35.4634, 44.8994, 46.5891, 38.8951, 58.3019, 61.2181, 
               37.7749, 38.5731, 46.5891, 38.3362, 39.9526, 46.5891, 
               39.7392),
-  cap_long = c(-122.3331, -86.1581, -71.5376, -77.46, -78.6382, -84.387, 
+  cap_long = c(-122.3331, -71.5376, -77.46, -78.6382, -84.387, 
                -77.0364, -96.7056, -104.9903, -73.7562, -76.7019, -77.0364, 
                -117.1611, -87.7406, -91.1403, -89.6811, -97.7431, -84.2708, 
                -112.0740, -82.9988, -157.8580, -72.6851, -71.0589, -122.8742, 
@@ -75,52 +75,43 @@ state_capitals <- data.frame(
                -84.0907, -112.0740, -95.7129, -77.0369, -111.8910, -75.3778, 
                -75.4694)) #gives the state capitals for each state
 
-washington_cars <- cars %>% 
-  mutate(Vehicle.Location = gsub("POINT \\((-?\\d+\\.\\d+) (-?\\d+\\.\\d+)\\)", "\\1,\\2", Vehicle.Location), # removes the POINT and () around the longitude and latitude
-         Vehicle.Location = ifelse(Vehicle.Location == "", NA, Vehicle.Location)) %>% # adds NA for missing location values
-  separate(Vehicle.Location, into = c("Longitude", "Latitude"), sep = ",") %>% # splits the values into latitude and longitude
-  filter(State == "WA") %>%
-  mutate(Latitude = as.numeric(Latitude),
-         Longitude = as.numeric(Longitude)) %>%  # changes longitude and latitude values to numeric
-  group_by(County, Make) %>% # group by the state and the make of the car
-  summarise(Count = n()) %>% # count the number of the occurrences of each make in each state
-  arrange(County, desc(Count)) %>% # select the car with the highest number of occurrences in each state
-  slice(1) %>%
-  ungroup()
+#Joined data
+joined_us <- left_join(us_cars, state_capitals, by = c("State" = "State"))
 
 
-#Final data for plotting ----
-cars_per_state <- new_cars %>%
-  left_join(state_capitals, by = c("State" = "State"))
+# Maps ----
+washington_state <- c(lat = 47.751076, lon = -120.740135) 
+washington_map <- get_map(location = washington_state, zoom = 6, source = "google", maptype = "terrain")
+(washington_ggmap <- ggmap(washington_map))
+joined$Make <- factor(joined$Make)
 
-#Plotting data
+# Plot 1
+(map1 <- ggmap(washington_map) +
+    geom_point(data = joined, aes(x = Longitude, y = Latitude, color = Make, shape = Make), size = 3) +
+    ggrepel::geom_label_repel(data = joined, aes(x = Longitude, y = Latitude, label = paste(County)),
+                              max.overlaps = 20, box.padding = 0.5, point.padding = 0.1, 
+                              segment.color = "black", size = 2) +
+    scale_color_manual(values = c("TESLA" = "orchid", "FORD" = "black", "CHEVROLET" = "darkblue"), guide = guide_legend(title = "Make")) +
+    scale_shape_manual(values = c("TESLA" = 15, "FORD" = 16, "CHEVROLET" = 17), guide = guide_legend(title = "Make")) +
+    xlab("Longitude") +
+    ylab("Latitude") +
+    theme(legend.position = c(0.85, 0.87)) +
+    labs(color = "Electric car make"))
+
+ggsave("test_map_wa.png", map1, units = "cm", width = 20, height = 17)
+
+
+# Plot 2
 usa <- map_data("world", region = c("USA", "Canada", 'Mexico'))
 
 ggplot() +
   geom_map(data = usa, map = usa, aes(x = long, y = lat, map_id = region),
            color = "gray80", fill = "gray80") +
-  geom_path(data = usa, aes(x = long, y = lat, group = group), color = "black", size = 0.3) +
-  geom_point(data = cars_per_state, aes(x = cap_long, y = cap_lat), 
+  geom_path(data = usa, aes(x = long, y = lat, group = group), color = "black", linewidth = 0.3) +
+  geom_point(data = joined_us, aes(x = cap_long, y = cap_lat), 
              alpha = 0.8, color = "red", size = 3) +
-  ggrepel::geom_label_repel(data = cars_per_state, aes(x = cap_long, y = cap_lat, label = Make),
+  ggrepel::geom_label_repel(data = joined_us, aes(x = cap_long, y = cap_lat, label = Make),
                             max.overlaps = 100, box.padding = 0.5, point.padding = 0.1, 
                             segment.color = "blue", size = 2) +
   coord_sf(xlim = c(-170, -60), ylim = c(10, 75))  +
   theme_void()
-
-
-library(ggplot2)
-
-# Create a map
-map <- ggplot(data = washington_cars, aes(x = Longitude, y = Latitude)) +
-  geom_point(aes(fill = Make), size = 3, shape = 21) +
-  # Customize the appearance of the map
-  map <- map +
-  xlab("Longitude") +
-  ylab("Latitude") +
-  theme(legend.position = c(0.85, 0.87),
-        legend.title = element_text(text = "Most Common Make")) +
-  guides(fill = guide_legend(override.aes = list(size = 3)))
-         
-         print(map)
-         
--- Weekly DS content/Weeks 6 - 8/DS - W6.R
+++ Weekly DS content/Weeks 6 - 8/DS - W6.R
@@ -5,6 +5,13 @@
 ##%#########################################################################%##
 
 #Libraries
+library(tidyverse)
+library(lme4)
+library(brms)
+library(MCMCglmm)
+library(broom)
+library(broom.mixed)
+library(ggeffects)
 
 #WD
 setwd("~/") #erases previously set WDs
@@ -12,4 +19,102 @@ setwd("DS - Personal repo - Zoja/Weekly DS content/Weeks 6 - 8") #sets a new one
 getwd() #check that it's worked
 
 #Data
+# Load data and wrangle #------------------------------------------------------------------------
+# Toolik data
+toolik_plants <- read_csv("weekly_materials/week_06-08/data/toolik_plants.csv")
 
+# Inspect data
+head(toolik_plants)
+str(toolik_plants) # What classes are the variables in the dataset?
+
+toolik_plants$Plot <- as.factor(as.character(toolik_plants$Plot))
+
+length(unique(toolik_plants$Site)) # How many sites are the data from?
+
+unique(toolik_plants$Year) # How many years does this data cover?
+
+# Remove non-species
+toolik_plants <- toolik_plants %>%
+  filter(!Species %in% c("Woody cover", "Tube",
+                         "Hole", "Vole trail",
+                         "removed", "vole turds",
+                         "Mushrooms", "Water",
+                         "Caribou poop", "Rocks",
+                         "mushroom", "caribou poop",
+                         "animal litter", "vole poop",
+                         "Vole poop", "Unk?"))
+
+# Calculate species richness
+toolik_plants <- toolik_plants %>%
+  group_by(Year, Site, Block, Plot) %>%
+  summarise(Richness = length(unique(Species))) %>% 
+  ungroup()
+
+# Explore the distribution of the data
+(hist2 <- ggplot(toolik_plants, aes(x = Richness)) +
+    geom_histogram() +
+    theme_classic())
+
+#Make a new column with a simplified year variable for use in models (2008 becomes year 1)
+toolik_plants$Year_simple <- toolik_plants$Year-2007
+
+
+
+# WEEK 6. Linear models #-------------------------------------------------------------------------
+
+## Gaussian family ####
+# Richness over time
+
+
+# Handy way of tidying model output!
+(model.table1 <- as.data.frame(tidy(name_of_your_model)))
+
+
+# QUESTIONS:
+
+# Write a summary sentence of how plant richness has changed over time
+
+# What is the rate of change?
+
+# What is the statistical power of the model? sample size = ? number of plots = ?
+
+# Does the model output change if you run the model again?
+
+# What values should you report?
+
+# What is the error around this model? Are you confident in it?
+
+# How is species richness changing over time?
+
+
+# PREDICTION:
+# What is a model prediction?
+
+# What is the predicted species richness in the year 2013?
+
+
+
+
+
+## Trying a different family ####
+# How did the Gaussian model fit? What is most appropriate distribution for these data? 
+
+# Fit a new model:
+
+
+# QUESTIONS:
+
+# How has this changed the model diagnostic plots? Does it look better?
+
+# How has this changed the model estimates? (The slope and intercept) 
+# Compare these numbers to the numbers from the Gaussian model. Why might there be differences?
+
+# Write a summary sentence of how plant richness has changed over time
+
+# What is the rate of change?
+
+# Which model do you trust more?!
+
+
+# PREDICTION: 
+# What is the predicted species richness in the year 2013?
