61414254dc4126215d702e343576bb7215c88563
zmancekpali <145681961+zmancekpali@users.noreply.github.com>
2023-11-02 13:14:01 +0000

    updates

--- Dissertation/Dissertation.R
+++ Dissertation/Dissertation.R
@@ -12,6 +12,7 @@ getwd() #check that it's worked
 
 
 #Libraries
+library(ape)
 library(cowplot)
 library(dunn.test)
 library(e1071)
@@ -21,6 +22,7 @@ library(gridExtra)
 library(lme4)
 library(MASS)
 library(multcomp)
+library(stats)
 library(tidyverse)
 library(vegan)
 
@@ -38,8 +40,10 @@ trees <- trees %>%
   filter(A >= 0) %>% 
   arrange(code_two = factor(type, levels = c('Native', 'Naturalised', 'Invasive', 
                                              'C. bullatus', 'Q. cerris', 'Q. ilex', 
-                                             'R. pseudoacacia semperflorens')))  #rearranges the categories in this order
+                                             'R. pseudoacacia semperflorens'))) #rearranges the categories in this order
 
+trees$age <- as.numeric(trees$age)
+  
 nns <- trees %>% 
   filter(type %in% c('Native', 'Naturalised', "Invasive")) %>% #excluding the alien group for initial analysis
   mutate(canopy_pos = recode(canopy_pos, 
@@ -322,6 +326,7 @@ ggsave("dr_boxplot.jpg", dr_boxplot, path = "Plots", units = "cm", width = 20, h
 
 
 
+
 #Step 2: compare alien species ----
 #LMA ----
 lma_mod2 <- lm(lma ~ code_two, data = trees)
@@ -342,6 +347,8 @@ bartlett.test(transformed_lma ~ type, data = trees) #heteroscedascity
 #Transformation did not work, moving on to non-parametric alternative:
 (lma_kw2 <- kruskal.test(lma ~ type, data = trees)) #p-value = 6.206e-07; significant
 
+
+
 (lma_boxplot2 <- ggplot(trees, 
                        aes(x = factor(code_two, levels = 
                                         c('Native', 'Naturalised', 'Invasive', 
@@ -352,18 +359,20 @@ bartlett.test(transformed_lma ~ type, data = trees) #heteroscedascity
     stat_boxplot(geom ='errorbar', width = 0.3) + #adds the whisker ends
     geom_jitter() + #adds the jitter
     scale_fill_manual(values = c("Invasive" = "#CD6090", "Native" = "#698B69",
-                                 "Naturalised" = "#EEC900", "C. bullatus" = "#5EA8D9",
-                                 "Q. cerris" = "#245C82", "Q. ilex" = "#4A3E87",
-                                 "R. pseudoacacia semperflorens" = "#5A5DC7")) + #colours each boxplot this particular colour
+                                 "Naturalised" = "#EEC900", "C. bullatus" = "steelblue3",
+                                 "Q. cerris" = "steelblue3", "Q. ilex" = "steelblue3",
+                                 "R. pseudoacacia semperflorens" = "steelblue3")) + #colours each boxplot this particular colour
     labs(x = "\n Invasion status", 
          y = expression(atop("LMA (g/cm"^2*")"))) + 
     theme_classic() + 
     theme(axis.text = element_text(size = 10), 
           axis.title = element_text(size = 11), 
           plot.margin = unit(c(0.5,0.5,0.5,0.5), units = , "cm"), 
-          legend.position = "none"))
+          legend.position = "top"))
 
 ggsave("lma_boxplot2.jpg", lma_boxplot2, units = "cm", width = 30, height = 15) 
+
+
 
 #Dunn post-hoc test
 dunn_lma_2 <- dunn.test(nns$lma, nns$code_2, method = "bonferroni") #invasives differ significantly from natives yay
@@ -372,14 +381,19 @@ dunn_lma_2 <- dunn.test(nns$lma, nns$code_2, method = "bonferroni") #invasives d
 
 
 
-#Step 3 - Mixed effect models ----
-#mixed effect models?? ----
+
+
+
+#Step 3 - Mixed effect models?? ----
+#random effect models?? ----
+null_lma <- lm(lma ~ 1, data = nns)
 model_lma <- lmer(lma ~ type + (1 | ever_dec), data = nns)
 model_lma_1 <- lmer(lma ~ type + (1 | code) + (1 | age) +  (1 | ever_dec), data = nns) 
 model_lma_2 <- lmer(lma ~ type + (1 | code) + (1 | age) +  (1 | ever_dec) + (1 | canopy_pos), data = nns)
 model_lma_3 <- lmer(lma ~ type + (1 | code) + (1 | age) +  (1 | ever_dec) + (1 | canopy_pos) + (1 | dbh), data = nns)
-AIC(model_lma_1, model_lma_2, model_lma_3)
-#models 2 and 3 fall within 2 AIC scores; so virtually identical fit to the data
+model_lma_4 <- lmer(lma ~ type + (1 | code) +  (1 | ever_dec) + (1 | canopy_pos), data = nns) #this one is best (accoridng to the AIC)
+AIC(null_lma, model_lma, model_lma_1, model_lma_2, model_lma_3, model_lma_4)
+#models 2 and 4 fall within 2 AIC scores; so virtually identical fit to the data
 #looks like adding DBH does not add anything to the data, so I will not use it
 
 #so for model_lma_2; there is still 12.56 residual std that is not explained by any of these random effects
@@ -404,4 +418,19 @@ lma_matrix <- lma_matrix %>%
 nmds_result <- metaMDS(lma_matrix, distance = "bray") #why bray??
 plot(nmds_result)
 
+#PCA ----
+pca_data <- trees %>% 
+  select(lma, ldcm, A, E, g, chl)
 
+pca <- princomp(pca_data, cor = TRUE, scores = TRUE)
+pca$loadings #pca1 explains loads -> add the scree plot and see that the comp 1 explains a lot (plot(pca))
+#lma and chl are the most important variables in the pca1; followied by g, A, and E (but the latter in the opp direction)
+#add the pca1 and pca2 to the original dataset to plot 
+#can go back to the models and use the pca as a resposne variable
+#then decide how to name the groups (i.e. pca1 = morphological and chemical; pca2 = morhological and physiological)
+
+biplot <- biplot(pca)
+
+#workflow
+#dimension reduction (pca) -> modelling -> post-hoc tests
+#double check that the traits are normally distributed for the PCA (if not, then transform)
