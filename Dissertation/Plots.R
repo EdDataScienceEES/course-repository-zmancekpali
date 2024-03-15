3bf6a8f1d4f0168714407dfb70fb1052cd492693
zmancekpali <145681961+zmancekpali@users.noreply.github.com>
2024-03-15 15:00:40 +0000

    maps finished!!

--- Dissertation/Plots.R
+++ Dissertation/Plots.R
@@ -22,7 +22,6 @@ ggmap::register_google(key = "AIzaSyDnersipSvcXuK4tCDbr8NOpa-qsrYf9pc",
 
 #Data
 leaves <- read.csv("traits_analysis.csv")
-leaves2 <- read.csv("traits_analysis2.csv")
 
 
 leaves <- leaves %>% 
@@ -33,116 +32,133 @@ leaves <- leaves %>%
                        "Native" = "Native species")) %>%  #recode the invasion type names
   distinct(long, lat, .keep_all = TRUE) #remove multiple rows (avoids overplotting)
 
-leaves2 <- leaves2 %>% 
-  select("type", "code", "latin_name", "long", "lat") %>%  #select the relevant columns
-  mutate(type = recode(type, "Alien" = "Alien species",
-                       "Invasive" = "Invasive species", 
-                       "Naturalised" = "Naturalised species", 
-                       "Native" = "Native species")) %>%  #recode the invasion type names
-  distinct(code, long, lat, .keep_all = TRUE) #remove multiple rows (avoids overplotting)
 
 
 #Maps
-(edinburgh <- get_googlemap("edinburgh", zoom = 14))
-rbge <- c(left = -3.21240, bottom = 55.948, right = -3.2025, top = 58.9682) #set the map view window accordingly; I want to view the Botanics
-edi_map_sattelite <- get_map(rbge, maptype = 'satellite', source = "google", zoom = 14) #specify what kind of map you want
-ggmap(edi_map_satellite)
-
-
-
-rbge_map <- get_googlemap("royal botanic gardens edinburgh", zoom = 16, maptype = "satellite")
-ggmap(rbge_map)
-
+rbge_map <- get_googlemap("Royal Botanic Gardens Edinburgh", zoom = 16, maptype = "satellite")
+#Simple RBGE map: ----
 (rbge_simple_map <- ggmap(rbge_map) +
-    geom_point(data = leaves2, aes(x = long, y = lat, color = type), 
-               size = 3, alpha = 0.5))
-
-(rbge_map <- ggmap(edi_map_satellite) +
-    geom_point(data = leaves, aes(x = lat, y = long, color = type, shape = type), 
-               size = 4) +
+    geom_point(data = leaves, aes(x = long, y = lat, color = type, shape = type), 
+               size = 3) +
     scale_color_manual(values = c("#5EA8D9", "#CC168F", "green", "#EEC900"),
                        name = "Invasion type") +
     scale_shape_manual(values = c(17, 15, 18, 16), name = "Invasion type") +
-    xlab("Latititude") +
-    ylab("Longitude") +
+    ylab("Latititude") +
+    xlab("Longitude") +
+    theme_void() +
     theme(legend.position = c(0.85, 0.87),
-          legend.key = element_rect(fill = "floralwhite"),
-          legend.background = element_rect(fill = "floralwhite")))
+          legend.key = element_rect(fill = "white", color = "white"),
+          legend.background = element_rect(fill = "white", color = "white"),
+          legend.key.size = unit(1.5, "line")) +
+    annotation_north_arrow(location = "tl", which_north = "true", 
+                           style = north_arrow_fancy_orienteering (text_col = 'white',
+                                                                   line_col = 'white',
+                                                                   fill = 'white')))
+ggsave("rbge_map_simple.jpg", rbge_simple_map, path = "Plots", units = "cm", 
+       width = 20, height = 20)
 
 
+#Map with species abbreviations: ----
+(abbr_map <- ggmap(rbge_map) +
+  geom_point(data = leaves, aes(x = long, y = lat, color = type, shape = type), 
+             size = 3) +
+  scale_color_manual(values = c("#5EA8D9", "#CC168F", "green", "#EEC900"),
+                     name = "Invasion type") +
+  scale_shape_manual(values = c(16, 17, 15, 18), name = "Invasion type") +
+  xlab("Longitude") +
+  ylab("Latitude") +
+  theme_void() +
+  theme(legend.position = c(0.85, 0.87),
+        legend.key = element_rect(fill = "white", color = "white"),
+        legend.background = element_rect(fill = "white", color = "white"),
+        legend.key.size = unit(1.5, "line")) +
+  ggrepel::geom_label_repel(data = leaves, aes(x = long, y = lat, label = code),
+                            max.overlaps = 50, box.padding = 0.5, 
+                            point.padding = 0.1, segment.color = "white", 
+                            size = 3, fontface = "plain") +
+  annotation_north_arrow(location = "tl", which_north = "true", 
+                         style = north_arrow_fancy_orienteering (text_col = 'white',
+                                                                 line_col = 'white',
+                                                                 fill = 'white')))
+ggsave("rbge_map_with_abbreviations.jpg", abbr_map, path = "Plots", units = "cm", 
+       width = 20, height = 20)
 
-(rbge_map_with_names <- ggmap(edi_map_satellite) +
-    geom_point(data = leaves, aes(x = lat, y = long, color = type, shape = type), 
+
+#Map with species names: ----
+(names_map <- ggmap(rbge_map) +
+    geom_point(data = leaves2, aes(x = long, y = lat, color = type, shape = type), 
                size = 3) +
     scale_color_manual(values = c("#5EA8D9", "#CD6090", "#2CB82E", "#EEC900"),
                        name = "Invasion type") +
     scale_shape_manual(values = c(16, 17, 18, 15), name = "Invasion type") +
-    ylab("Longitude") +
-    xlab("Latitude") +
-    theme(legend.position = c(0.85, 0.87),
-          legend.key = element_rect(fill = "floralwhite"),
-          legend.background = element_rect(fill = "floralwhite")) +
-    ggrepel::geom_label_repel(data = leaves, aes(x = lat, y = long, label = latin_name),
-                              max.overlaps = 200, box.padding = 0.5, point.padding = 0.1, 
-                              segment.color = "floralwhite", size = 3, fontface = "italic") +
-    annotation_north_arrow(location = "tl", which_north = "true", 
-                           style = north_arrow_fancy_orienteering (text_col = 'floralwhite',
-                                                                   line_col = 'floralwhite',
-                                                                   fill = 'floralwhite')))
-ggsave("rbge_map_with_names.jpg", rbge_map_with_names, path = "Plots", units = "cm", 
-       width = 30, height = 20)
-
-(map_with_codes <- ggmap(edi_map_satellite) +
-    geom_point(data = leaves2, aes(x = long, y = lat, color = type, shape = type), 
-               size = 3) +
-    scale_color_manual(values = c("#5EA8D9", "#CD6090", "#698B69", "#EEC900"),
-                       name = "Invasion type") +
-    scale_shape_manual(values = c(16, 17, 18, 15), name = "Invasion type") +
     xlab("Longitude") +
     ylab("Latitude") +
+    theme_void() +
     theme(legend.position = c(0.85, 0.87),
-          legend.key = element_rect(fill = "floralwhite"),
-          legend.background = element_rect(fill = "floralwhite")) +
-    ggrepel::geom_label_repel(data = leaves2, aes(x = long, y = lat, label = code),
-                              max.overlaps = 300, box.padding = 0.5, 
-                              point.padding = 0.1, segment.color = "floralwhite", 
-                              size = 3, fontface = "plain") +
+          legend.key = element_rect(fill = "white", color = "white"),
+          legend.background = element_rect(fill = "white", color = "white"),
+          legend.key.size = unit(1.5, "line")) +
+    ggrepel::geom_label_repel(data = leaves, aes(x = long, y = lat, label = latin_name),
+                              max.overlaps = 50, box.padding = 0.5, point.padding = 0.1, 
+                              segment.color = "white", size = 2.5, fontface = "italic") +
     annotation_north_arrow(location = "tl", which_north = "true", 
-                           style = north_arrow_fancy_orienteering (text_col = 'floralwhite',
-                                                                   line_col = 'floralwhite',
-                                                                   fill = 'floralwhite')))
+                           style = north_arrow_fancy_orienteering (text_col = 'white',
+                                                                   line_col = 'white',
+                                                                   fill = 'white')))
+ggsave("rbge_map_with_names.jpg", names_map, path = "Plots", units = "cm", 
+       width = 20, height = 20)
 
-ggsave("map_with_codes.jpg", map_with_codes, path = "Plots", units = "cm", 
-       width = 30, height = 20)
+#Edinburgh map: ----
+edinburgh_map <- get_googlemap("Edinburgh", zoom = 12, maptype = "satellite")
 
-(rbge_grid <- grid.arrange(rbge_map_with_names, map_with_codes, ncol = 2))
-ggsave("rbge_grid.jpg", rbge_grid, path = "Plots", units = "cm", width = 50,
-       height = 20)
+(edi_10zoom <- ggmap(edinburgh_map) +
+    geom_point(data = NULL, aes(x = -3.209664, y = 55.965140), color = "red", 
+               size = 3, shape = 17) +
+    theme_void() +
+    annotation_north_arrow(location = "tl", which_north = "true", 
+                           style = north_arrow_fancy_orienteering (text_col = 'white',
+                                                                   line_col = 'white',
+                                                                   fill = 'white')))
+ggsave("edi_map(10).jpg", edi_10zoom, path = "Plots", units = "cm", 
+       width = 20, height = 20)
 
-#trying somethig else here:
-library("rnaturalearth")
-library("rnaturalearthdata")
-library(maps)
 
-worldmap = map_data('world')
+(edi_11zoom <- ggmap(edinburgh_map) +
+    geom_point(data = NULL, aes(x = -3.209664, y = 55.965140), color = "red", 
+                                        size = 3, shape = 17) +
+    theme_void() +
+    annotation_north_arrow(location = "tl", which_north = "true", 
+                           style = north_arrow_fancy_orienteering (text_col = 'white',
+                                                                   line_col = 'white',
+                                                                   fill = 'white')))
+ggsave("edi_map(11).jpg", edi_11zoom, path = "Plots", units = "cm", 
+       width = 20, height = 20)
 
-rbge <- c(left = -3.21240, bottom = 55.948, right = -3.2025, top = 58.9682) #set the map view window accordingly; I want to view the Botanics
-ggplot() + 
-  geom_polygon(data = worldmap, 
-               aes(x = long, y = lat, group = group),
-               fill = 'gray90', 
-               color = 'black') +
-  theme_classic() +
-  ylab("Latitude") +
-  xlab("Longitude") +
-  coord_fixed(ratio = 1.3,
-              xlim = c(-3.215, -3.2025), 
-              ylim = c(55.948, 55.9682)) +
-  geom_point(data = leaves2, 
-             aes(x = long, 
-                 y = lat, size = 3), alpha = .7) + 
-  scale_size_area(max_size = 8) + 
-  scale_color_viridis_c() + 
-  theme(legend.position = 'none') + 
-  theme(title = element_text(size = 12))
+(edi_12zoom <- ggmap(edinburgh_map) +
+    geom_point(data = NULL, aes(x = -3.209664, y = 55.965140), color = "red", 
+               size = 3, shape = 17) +
+    theme_void() +
+    annotation_north_arrow(location = "tl", which_north = "true", 
+                           style = north_arrow_fancy_orienteering (text_col = 'white',
+                                                                   line_col = 'white',
+                                                                   fill = 'white')))
+ggsave("edi_map(12).jpg", edi_12zoom, path = "Plots", units = "cm", 
+       width = 20, height = 20)
+
+
+
+#Scotland map ----
+scotland_map <- get_googlemap("Scotland", zoom = 7, maptype = "satellite")
+
+(scotland <- ggmap(scotland_map) +
+  geom_point(data = NULL, aes(x = -3.209664, y = 55.965140), color = "red", 
+             size = 3, shape = 17) +
+  theme_void() +
+  annotation_north_arrow(location = "tl", which_north = "true", 
+                         style = north_arrow_fancy_orienteering (text_col = 'white',
+                                                                 line_col = 'white',
+                                                                 fill = 'white')))
+
+ggsave("scotland_map.jpg", scotland, path = "Plots", units = "cm", 
+       width = 20, height = 20)
 
